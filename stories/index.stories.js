1faa9bc48a7d8d4d8c0a46bc5419f66e80f4fd92
Igor Guastalla <limaguastallaigor@gmail.com>
2020-09-24 18:37:17 -0300

    refactor: refactor storybook stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,154 +1,217 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { storiesOf } from '@storybook/vue'
-import { withKnobs, object, text, select, boolean, number } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import VueListPicker from '../src/components/VueListPicker'
 
-const listpickerStory = storiesOf('VueListPicker', module)
-  .addParameters({ backgrounds: [
-    { name: 'Blue', value: 'blue' },
-    { name: 'Green', value: 'green' },
-    { name: 'Yellow', value: 'yellow' },
-    { name: 'Orange', value: 'orange' },
-    { name: 'Red', value: 'red' },
-    { name: 'Purple', value: 'purple' },
-    { name: 'Black', value: 'black' },
-    { name: 'White', value: 'white', default: true }
-  ] })
-  .addDecorator(withKnobs)
-
-listpickerStory.add('Default', () => {
-  const label1 = 'Object 1'
-  const defaultValue1 = {
-    key: 1,
-    content: 'Item 1'
-  }
-  const value1 = object(label1, defaultValue1)
-
-  const label2 = 'Object 2'
-  const defaultValue2 = {
-    key: 2,
-    content: 'Item 2'
-  }
-  const value2 = object(label2, defaultValue2)
-
-  const label3 = 'Object 3'
-  const defaultValue3 = {
-    key: 3,
-    content: 'Item 3'
-  }
-  const value3 = object(label3, defaultValue3)
-
-  const leftItems = [value1, value2, value3]
-  const rightItems = []
-
-  return {
-    components: { VueListPicker },
-    methods: {
-      moveLeft: action('move-left'),
-      moveRight: action('move-right'),
-      moveAllLeft: action('move-all-left'),
-      moveAllRight: action('move-all-right'),
-      unselectAll: action('unselect-all')
+export default {
+  title: 'VueListPicker',
+  component: VueListPicker,
+  parameters: {
+    controls: {
+      hideNoControlsWarning: true,
+      expanded: true
     },
-    props: {
-      leftItems: {
-        type: Array,
-        default: [ ...leftItems.map(it => ({ ...it })) ]
-      },
-      rightItems: {
-        type: Array,
-        default: [ ...rightItems.map(it => ({ ...it })) ]
-      },
-      movedItemLocation: {
-        type: String,
-        default: select('Move item to top or bottom', { top: 'top', bottom: 'bottom' }, 'top')
-      },
-      titleLeft: {
-        type: String,
-        default: text('Title of the left column', 'Items available')
-      },
-      titleRight: {
-        type: String,
-        default: text('Title of the right column', 'Items selected')
-      },
-      titleCentered: {
-        type: Boolean,
-        default: boolean('Title centered text', true)
-      },
-      titleClass: {
-        type: String,
-        default: text('Title custom class', '')
-      },
-      titleSubstr: {
-        type: Number,
-        default: number('Title substring', 20)
-      },
-      buttonClass: {
-        type: String,
-        default: text('Button custom class', '')
-      },
-      contentKey: {
-        type: String,
-        default: text('Content attribute key', 'key')
-      },
-      contentAttr: {
-        type: String,
-        default: text('Content attribute text', 'content')
-      },
-      contentCentered: {
-        type: Boolean,
-        default: boolean('Content centered text', false)
-      },
-      contentClass: {
-        type: String,
-        default: text('Content custom class', '')
-      },
-      contentSubstr: {
-        type: Number,
-        default: number('Content substring', 23)
-      },
-      minHeight: {
-        type: String,
-        default: text('Min height of each column', '450px')
-      },
-      height: {
-        type: String,
-        default: text('Height of each column', '')
-      },
-      minWidth: {
-        type: String,
-        default: text('Min width of each column', '220px')
-      },
-      width: {
-        type: String,
-        default: text('Width of each column', '')
+    backgrounds: {
+      default: 'White',
+      values: [
+        { name: 'Blue', value: 'blue' },
+        { name: 'Green', value: 'green' },
+        { name: 'Yellow', value: 'yellow' },
+        { name: 'Orange', value: 'orange' },
+        { name: 'Red', value: 'red' },
+        { name: 'Purple', value: 'purple' },
+        { name: 'Black', value: 'black' },
+        { name: 'White', value: 'white' }
+      ]
+    }
+  },
+  argTypes: {
+    leftItems: {
+      control: 'array'
+    },
+    rightItems: {
+      control: 'array'
+    },
+    movedItemLocation: {
+      control: {
+        type: 'inline-radio',
+        options: ['top', 'bottom'],
       }
     },
-    template: `<vue-list-picker
-      :left-items="leftItems"
-      :right-items="rightItems"
-      :moved-item-location="movedItemLocation"
-      :title-left="titleLeft"
-      :title-right="titleRight"
-      :title-centered="titleCentered"
-      :title-class="titleClass"
-      :title-substr="titleSubstr"
-      :button-class="buttonClass"
-      :content-key="contentKey"
-      :content-attr="contentAttr"
-      :content-centered="contentCentered"
-      :content-class="contentClass"
-      :content-substr="contentSubstr"
-      :min-height="minHeight"
-      :height="height"
-      :min-width="minWidth"
-      :width="width"
-      @move-left="moveLeft"
-      @move-right="moveRight"
-      @move-all-right="moveAllRight"
-      @move-all-left="moveAllLeft"
-      @unselect-all="unselectAll"
-      />`
+    titleLeft: {
+      control: 'text'
+    },
+    titleRight: {
+      control: 'text'
+    },
+    titleCentered: {
+      control: 'boolean'
+    },
+    titleClass: {
+      control: 'text'
+    },
+    titleSubstr: {
+      control: 'number'
+    },
+    buttonClass: {
+      control: 'text'
+    },
+    contentKey: {
+      control: 'text'
+    },
+    contentAttr: {
+      control: 'text'
+    },
+    contentCentered: {
+      control: 'boolean'
+    },
+    contentClass: {
+      control: 'text'
+    },
+    contentSubstr: {
+      control: 'number'
+    },
+    minHeight: {
+      control: 'text'
+    },
+    height: {
+      control: 'text'
+    },
+    minWidth: {
+      control: 'text'
+    },
+    width: {
+      control: 'text'
+    }
   }
+}
+
+const example1 = {
+  key: 1,
+  content: 'Item 1'
+}
+const example2 = {
+  key: 2,
+  content: 'Item 2'
+}
+const example3 = {
+  key: 3,
+  content: 'Item 3'
+}
+const example4 = {
+  key: 4,
+  content: 'Item 4'
+}
+
+const leftItems = [example1, example2]
+const rightItems = [example3, example4]
+
+const actionsData = {
+  moveLeft: action('move-left'),
+  moveRight: action('move-right'),
+  moveAllLeft: action('move-all-left'),
+  moveAllRight: action('move-all-right'),
+  unselectAll: action('unselect-all')
+};
+
+export const Default = () => ({
+  components: { VueListPicker },
+  methods: actionsData,
+  props: {
+    leftItems: {
+      type: Array,
+      default: leftItems
+    },
+    rightItems: {
+      type: Array,
+      default: rightItems
+    },
+    movedItemLocation: {
+      type: String,
+      default: 'top'
+    },
+    titleLeft: {
+      type: String,
+      default: 'Items available'
+    },
+    titleRight: {
+      type: String,
+      default: 'Items selected'
+    },
+    titleCentered: {
+      type: Boolean,
+      default: true
+    },
+    titleClass: {
+      type: String,
+      default: ''
+    },
+    titleSubstr: {
+      type: Number,
+      default: 20
+    },
+    buttonClass: {
+      type: String,
+      default: ''
+    },
+    contentKey: {
+      type: String,
+      default: 'key'
+    },
+    contentAttr: {
+      type: String,
+      default: 'content'
+    },
+    contentCentered: {
+      type: Boolean,
+      default: false
+    },
+    contentClass: {
+      type: String,
+      default: ''
+    },
+    contentSubstr: {
+      type: Number,
+      default: 23
+    },
+    minHeight: {
+      type: String,
+      default: '450px'
+    },
+    height: {
+      type: String,
+      default: ''
+    },
+    minWidth: {
+      type: String,
+      default: '220px'
+    },
+    width: {
+      type: String,
+      default: ''
+    }
+  },
+  template: `<vue-list-picker
+    :left-items="leftItems"
+    :right-items="rightItems"
+    :moved-item-location="movedItemLocation"
+    :title-left="titleLeft"
+    :title-right="titleRight"
+    :title-centered="titleCentered"
+    :title-class="titleClass"
+    :title-substr="titleSubstr"
+    :button-class="buttonClass"
+    :content-key="contentKey"
+    :content-attr="contentAttr"
+    :content-centered="contentCentered"
+    :content-class="contentClass"
+    :content-substr="contentSubstr"
+    :min-height="minHeight"
+    :height="height"
+    :min-width="minWidth"
+    :width="width"
+    @move-left="moveLeft"
+    @move-right="moveRight"
+    @move-all-right="moveAllRight"
+    @move-all-left="moveAllLeft"
+    @unselect-all="unselectAll"
+    />`
 })
