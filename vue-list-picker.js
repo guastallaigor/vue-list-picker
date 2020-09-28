37d7cf636bfcbacd03ab654b4b4838ebb82b69ee
Igor Guastalla <limaguastallaigor@gmail.com>
2020-09-28 15:36:34 -0300

    chore: improve install vue list picker component

--- vue-list-picker.js
+++ vue-list-picker.js
@@ -1,22 +1,36 @@
-import VueListPicker from './src/components/VueListPicker.vue'
+// Import vue component
+import component from './src/components/VueListPicker.vue';
 
-// Export components
-const Components = {
-  VueListPicker
+// install function executed by Vue.use()
+const install = function(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('VueListPicker', component);
+};
+
+// Create module definition for Vue.use()
+const plugin = {
+  install,
+};
+
+// To auto-install when vue is found
+// eslint-disable-next-line no-redeclare
+/* global window, global */
+let GlobalVue = null;
+
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
 }
 
-const VueListPickerPlugin = {
-  install (Vue) {
-    Object.keys(Components).forEach((name) => {
-      Vue.component(name, Components[name])
-    })
-  }
+if (GlobalVue) {
+  GlobalVue.use(plugin);
 }
 
-// Export as a plugin
-export default VueListPickerPlugin
+// Inject install function into component - allows component
+// to be registered via Vue.use() as well as Vue.component()
+component.install = install;
 
-// Export as individual components
-export {
-  VueListPicker
-}
+// Export component by default
+export default component;
