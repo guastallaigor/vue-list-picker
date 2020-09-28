2fb03bf342aa348b7a528ee9d2dc6889ae9142f0
Igor Guastalla <limaguastallaigor@gmail.com>
2020-09-28 15:49:06 -0300

    style: fix standard style

--- vue-list-picker.js
+++ vue-list-picker.js
@@ -1,36 +1,36 @@
 // Import vue component
-import component from './src/components/VueListPicker.vue';
+import component from './src/components/VueListPicker.vue'
 
 // install function executed by Vue.use()
-const install = function(Vue) {
-  if (install.installed) return;
-  install.installed = true;
-  Vue.component('VueListPicker', component);
-};
+const install = function (Vue) {
+  if (install.installed) return
+  install.installed = true
+  Vue.component('VueListPicker', component)
+}
 
 // Create module definition for Vue.use()
 const plugin = {
-  install,
-};
+  install
+}
 
 // To auto-install when vue is found
 // eslint-disable-next-line no-redeclare
 /* global window, global */
-let GlobalVue = null;
+let GlobalVue = null
 
 if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
+  GlobalVue = window.Vue
 } else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
+  GlobalVue = global.Vue
 }
 
 if (GlobalVue) {
-  GlobalVue.use(plugin);
+  GlobalVue.use(plugin)
 }
 
 // Inject install function into component - allows component
 // to be registered via Vue.use() as well as Vue.component()
-component.install = install;
+component.install = install
 
 // Export component by default
-export default component;
+export default component
